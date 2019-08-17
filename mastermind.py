09274732eda48a6c856654a83267d9b32f6ea24e
Manav Shah <shahmanav789@gmail.com>
2019-08-17 18:31:23 -0400

    working prototype
    game functions without any known bugs. code still needs much polishing however.

--- mastermind.py
+++ mastermind.py
@@ -1,17 +1,20 @@
 import pygame, sys, random, time, pygame.freetype
 
-#Colors  (R  ,G  ,B  )
+#Colors       (R  ,G  ,B  )
+#can be used in the secret code and guesses
 RED         = (227,2  ,2  )
 GREEN       = (30 ,217,55 )
 BLUE        = (21 ,115,237)
 YELLOW      = (240,252,13 )
 PINK        = (252,35 ,158)
 BROWN       = (125,55 ,76 )
+#can be used for responses to guesses
 BLACK       = (0  ,0  ,0  )
 WHITE       = (255,255,255)
+#for miscellaneous GUI uses
 GRAY        = (128,128,128)
+DARKERBROWN = (158,100,100)
 DARKERGREEN = (23, 179, 44)
-
 
 #Coordinate values
 ROW_HEIGHT=72
@@ -30,9 +33,8 @@ class Circle:
         color=toggle_dict[self.color]
         self.color=color
         self.rect=pygame.draw.circle(DISPLAYSURF,color,self.coordinate,self.radius)
-        pygame.display.update()
+        pygame.display.update(self.rect)
             
-
 def get_random_secret_code(listofcolors,number):
     return tuple(random.choices(listofcolors,k=number))
 
@@ -50,15 +52,13 @@ def check_solution(code):
             isexactmatch.append(False)
     code = [code[i] for i in range(4) if isexactmatch[i]==False]
     secretcode = [secretcode[i] for i in range(4) if isexactmatch[i]==False]
-    for color in code:
+    for color in set(code):
         n = min(code.count(color),secretcode.count(color))
         for i in range(n):
             response.append(BLACK)
     random.shuffle(response)
     return response
             
-            
-
 def draw_grid():
     '''Draws a grid of black lines onto background. x,y coordinates are hardcoded'''
     for i in range(11):
@@ -69,9 +69,12 @@ def show_secret_code(code):
     '''Displays the secret code'''
     for i,color in enumerate(code): 
         pygame.draw.circle(DISPLAYSURF,color,(130+(30*(i+1))+(i*50),36),25)
+        pygame.display.update()
         
 def victory_animation(background):
-    time.sleep(2)
+    show_secret_code(SECRETCODE)
+    pygame.display.update()
+    time.sleep(1.2)
     DISPLAYSURF.blit(background,(0,0))
     imageSurf = pygame.image.load('assets\\einstein.png')
     imageSurf.convert_alpha()
@@ -81,24 +84,78 @@ def victory_animation(background):
     fontObj=pygame.freetype.SysFont('comicsansms',26,bold=True)
     fontObj.render_to(DISPLAYSURF,(77,110),"You've cracked the code!")
     pygame.display.update()
+    pygame.mixer.music.load('assets\\victory.mp3')
+    pygame.mixer.music.play()
     time.sleep(3)
-#    for i in range(0,255,1):
-#        imageSurf.set_alpha(i)
-#        DISPLAYSURF.blit(imageSurf,(0,300))
-#        pygame.display.update()
-#        FPSClock.tick(FPS)
+    pygame.mixer.music.stop()
     pygame.quit()
     sys.exit()
     
+def losing_animation(background):
+    show_secret_code(SECRETCODE)
+    pygame.display.update()
+    time.sleep(0.5)
+    DISPLAYSURF.blit(background,(0,0))
+    fontObj=pygame.freetype.SysFont('comicsansms',40,bold=True)
+    fontObj.render_to(DISPLAYSURF,(95,50),'You just lost!')
+    pygame.display.update()
+    time.sleep(2)
+    pygame.quit()
+    sys.exit()
+    
+def draw_response(response,attempts_left):
+    '''Turns pre-shuffled response list to drawings. Draws circles in correct position based on how many attempts left'''
+    attempt=10-attempts_left
+    pygame.draw.rect(DISPLAYSURF,DARKERBROWN,(8,732-(72*attempt),85,51))
+    responseCoordinates = [(33,48+(72*attempts_left)),(33,24+(72*attempts_left)),(66,48+(72*attempts_left)),(66,24+(72*attempts_left))]
+    for color,coordinate in zip(response,responseCoordinates):
+        pygame.draw.circle(DISPLAYSURF,color,coordinate,10)
+    pygame.display.update()
+    time.sleep(1)
 
- 
-FPS = 60
+def draw_row(colors,attemptsleft):
+    for i,color in enumerate(colors):
+        pygame.draw.circle(DISPLAYSURF,color,((130+(30*(i+1))+(i*50)),36+(attemptsleft*72)),25)
+
+def screen_refresh(guessHistory,responseHistory,background):
+    DISPLAYSURF.blit(background,(0,0))
+    draw_grid()
+    for index,guess in enumerate(guessHistory):
+        position=10-index
+        draw_row(guess,position)
+    for index,response in enumerate(responseHistory):
+        position=10-index
+        draw_response(response,position)
+
+def move_to_next_line(guessHistory,attemptsleft):
+    latestcolors = guessHistory[-1]
+    draw_row(latestcolors,attemptsleft)
+
+def new_submit_box(submitbox,attemptsleft):
+    attempt = 11 - attemptsleft
+    submitbox = pygame.draw.rect(DISPLAYSURF,GREEN,(8,732-(72*attempt),85,51))
+    #Text label for the submit box
+    font=pygame.font.SysFont('Arial',25)
+    text = font.render('SUBMIT',True,WHITE)
+    DISPLAYSURF.blit(text,(11,25+((attemptsleft-1)*72)))
+    return submitbox
+    
+
+def new_active_row(activerow,attemptsleft):
+    del activerow[:]
+    for i in range(4):
+        circle=Circle(RED,(130+(30*(i+1))+(i*50)),36+((attemptsleft)*72),25)
+        activerow.append(circle)
+    
+    
+FPS = 100
 COLORS = [RED,GREEN,BLUE,YELLOW,PINK,BROWN]
-SECRETCODE = get_random_secret_code(COLORS,4)
+#SECRETCODE = get_random_secret_code(COLORS,4)
+SECRETCODE = (PINK,PINK,BLUE,GREEN)
     
 
 def main():
-    
+    attempts_left = 10
     pygame.init()
     #Crate a display with background
     global DISPLAYSURF
@@ -111,6 +168,11 @@ def main():
     global FPSClock
     FPSClock = pygame.time.Clock()
     
+    
+    #create black lined grid
+    draw_grid()
+    
+    
     #Create a submit box 
     submitbox = pygame.draw.rect(DISPLAYSURF,GREEN,(8,732,85,51))
     #Text label for the submit box
@@ -118,13 +180,9 @@ def main():
     text = font.render('SUBMIT',True,WHITE)
     DISPLAYSURF.blit(text,(11,745))
     
-    #create black lined grid
-    draw_grid()
-    
-    #testing
-    x=Circle(RED,30,30,25)
-    show_secret_code(SECRETCODE)
-    
+    #Create lists to store guesses and response history
+    guessHistory = []
+    responseHistory = []
     #Create interactive row
     activerow = []
     for i in range(4):
@@ -142,25 +200,34 @@ def main():
                 print(mousex,mousey)
             elif event.type == pygame.MOUSEBUTTONUP:
                 clickx,clicky = event.pos
-                if x.rect.collidepoint(clickx,clicky):
-                    x.toggle_color()
                 for circle in activerow:
                     if circle.rect.collidepoint(clickx,clicky):
                         circle.toggle_color()
                 if submitbox.collidepoint(clickx,clicky):
-                    submitbox=submitbox = pygame.draw.rect(DISPLAYSURF,DARKERGREEN,(8,732,85,51))
+                    #Submit button is clicked
                     activecolors = [circle.color for circle in activerow]
                     response=check_solution(activecolors)
+                    guessHistory.append(activecolors)
+                    responseHistory.append(response)
+                    submitbox=new_submit_box(submitbox,attempts_left)
+                    draw_response(response,attempts_left)
+                    attempts_left-=1
+                    if attempts_left==0:
+                        show_secret_code(SECRETCODE)
+                        losing_animation(background)
+#                    screen_refresh(guessHistory,responseHistory,background)
                     if response.count(WHITE)==4:
+                        show_secret_code(SECRETCODE)
                         victory_animation(background)
+                    new_active_row(activerow,attempts_left)
             
             #Handle mouse movement over submit box
             if submitbox.collidepoint(mousex,mousey):
-                submitbox=submitbox = pygame.draw.rect(DISPLAYSURF,DARKERGREEN,(8,732,85,51))
-                DISPLAYSURF.blit(text,(11,745))
+                submitbox=submitbox = pygame.draw.rect(DISPLAYSURF,DARKERGREEN,submitbox)
+                DISPLAYSURF.blit(text,(11,745-(72*(10-attempts_left))))
             else:
-                submitbox=submitbox = pygame.draw.rect(DISPLAYSURF,GREEN,(8,732,85,51))
-                DISPLAYSURF.blit(text,(11,745))
+                submitbox=submitbox = pygame.draw.rect(DISPLAYSURF,GREEN,submitbox)
+                DISPLAYSURF.blit(text,(11,745-(72*(10-attempts_left))))
             
                     
         pygame.display.update()
@@ -169,6 +236,3 @@ def main():
 
 if __name__=='__main__':
     main()
-
-
-
